8a8009e7e8cc57d52892bd20898cb73abe0f6c46
Sumant Pandey <sumantpandey529@gmail.com>
2026-01-16 08:18:48 +0530

    feat: add cookie-parser and cors middleware; implement async error handling and response classes

--- baatcheet-backend/src/index.js
+++ baatcheet-backend/src/index.js
@@ -1,7 +1,7 @@
 //require("dotenv").config({path: "./env"});
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
-
+import { app } from "./app.js";
 
 dotenv.config({
     path: './.env'
@@ -10,4 +10,12 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`App is listening on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
