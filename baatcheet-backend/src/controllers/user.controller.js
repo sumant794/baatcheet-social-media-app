7c78c325ea33a5d04c64e588223d6417bfe5b48a
Sumant Pandey <sumantpandey529@gmail.com>
2026-01-17 10:10:25 +0530

    feat: implement access and refresh token generation in login and registration processes

--- baatcheet-backend/src/controllers/user.controller.js
+++ baatcheet-backend/src/controllers/user.controller.js
@@ -3,6 +3,22 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAcessToken()
+        const refreshToken = user.generateRefreshToken()
+    
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave:false })
+    
+        return {accessToken, refreshToken}
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating tokens")
+    }
+}
+
 const registerUser = asyncHandler(async(req, res) => {
     const {username, email, fullName, password} = req.body
 
@@ -37,6 +53,48 @@ const registerUser = asyncHandler(async(req, res) => {
     .status(201)
     .json(
         new ApiResponse(201, createdUser, "User registered succesfully")
+    )
+
+})
+
+const loginUser = asyncHandler(async(req, res) => {
+    const {username, email, password} = req.body
+
+    if(!username && !email){
+        throw new ApiError(400, "Username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or:[{ username }, { email }]
+    })
+
+    if(!user){
+        throw new ApiError(404, "User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(401, "Invalid User Credentials")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefreshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    const options = {
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(200, loggedInUser, "User Logged In Succesfully")
     )
 
 })
