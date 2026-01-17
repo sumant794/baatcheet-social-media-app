91031a26d51be1a1d8fba6cd22c0269465f8a189
Sumant Pandey <sumantpandey529@gmail.com>
2026-01-17 11:04:57 +0530

    feat: implement JWT verification middleware and logout functionality

--- baatcheet-backend/src/controllers/user.controller.js
+++ baatcheet-backend/src/controllers/user.controller.js
@@ -99,10 +99,39 @@ const loginUser = asyncHandler(async(req, res) => {
 
 })
 
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset:{
+                refreshToken:1
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accesToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged Out Succesfully"))
+})
 
 
 
 
 
 
-export {registerUser}
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
