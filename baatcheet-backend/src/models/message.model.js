d3a2116dad20f0634807c2604f5cc0489d8a2429
Sumant Pandey <sumantpandey529@gmail.com>
2026-02-17 09:54:04 +0530

    Created message model

---
+++ baatcheet-backend/src/models/message.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose"
+
+const messageSchema = new Schema(
+    {
+        conversationId: {
+            type: Schema.Types.ObjectId,
+            ref: "Conversation",
+            required: true,
+            index: true,
+        },
+        senderId: {
+            type: Schema.Types.ObjectId,
+            ref:"User",
+            required: true,
+        },
+        text: {
+            type: String,
+            trim: true,
+            default: ""
+        },
+        messageType: {
+            type: String,
+            enum: ["text", "image", "video", "audio", "file"],
+            default: "text"
+        },
+        isSeen: {
+            type: Boolean,
+            default: false,
+        },
+        seenAt: {
+            type: Date,
+        }
+}, { timestamps: true})
+
+export const Message = mongoose.model("Message", messageSchema);
