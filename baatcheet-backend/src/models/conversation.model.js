3e9874da0f0af9cec83088100a3cfe098053bf5b
Sumant Pandey <sumantpandey529@gmail.com>
2026-02-17 07:41:43 +0530

    Created conversation model for chat meaasge feature

---
+++ baatcheet-backend/src/models/conversation.model.js
@@ -0,0 +1,20 @@
+import mongoose, {Schema} from "mongoose";
+
+const conversationSchema = new Schema({
+    members: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    ],
+    lastMessage: {
+        type: String,
+        default: "",
+    },
+    lastMessageAt: {
+        type: Date,
+    }
+}, { timestamps: true})
+
+export const Conversation = mongoose.model("Conversation", conversationSchema)
