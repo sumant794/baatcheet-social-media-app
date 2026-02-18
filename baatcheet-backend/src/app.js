dec2149229b7b848656bc086f507f3bfce45d2f8
Sumant Pandey <sumantpandey529@gmail.com>
2026-02-19 00:04:31 +0530

    Updated cors origin

--- baatcheet-backend/src/app.js
+++ baatcheet-backend/src/app.js
@@ -4,20 +4,20 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:5173",
-//       "http://10.179.197.252:5173"
-//     ],
-//     credentials: true
-//   })
-// );
-
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://10.179.197.252:5173"
+    ],
     credentials: true
-}))
+  })
+);
+
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
