a666a51496d0b067831338eff68e3a000bc8f317
Sumant Pandey <sumantpandey529@gmail.com>
2026-03-14 07:23:00 +0530

    Socket.js host change

--- baatcheet-backend/src/socket/socket.js
+++ baatcheet-backend/src/socket/socket.js
@@ -7,7 +7,7 @@ export const initSocket = (server) => {
   io = new Server(server, {
     cors: {
       origin: [
-        "http://localhost:5173",
+        "http://localhost:5175",
         "http://10.179.197.252:5173",
         "https://your-frontend-domain.com", // later replace
       ],
